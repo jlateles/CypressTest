62d3b4a1a8a865a9e59c312a1a2a2374a4a8ff4e
jlateles <juliatelesicm@gmail.com>
2024-07-12 18:16:40 -0300

    testes de cadastro

--- cypress/e2e/testeCadastro.cy.js
+++ cypress/e2e/testeCadastro.cy.js
@@ -2,14 +2,13 @@ describe('Página de cadastro', () => {
   it('Deve preencher os campos do formulário corretamente para cadatsrar um novo usuário', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
     cy.wait(2000); // Pausa de 2 segundos
-    cy.get('[data-test="register-button]').click();
+    cy.get('[data-test="register-button"]').click();
     cy.get('[data-test="input-name"]').type('Julia Teles da Silva Bispo');
-    cy.get ('[data-test="input-email"]').type('juliabispo@gmail.com');
+    cy.get('[data-test="input-email"]').type('juliabispo@gmail.com');
     cy.get('[data-test="input-password"]').type('Bispo@1234');
-    cy.get('[data-test="input-confirm_password"]').type('Bispo@12345');
+    cy.get('[data-test="input-confirm-password"]').type('Bispo@1234');
     cy.wait(2000); // Pausa de 2 segundos
-    cy.contains('[data-test="submit-button]').click();
-    cy.wait(2000); // Pausa de 2 segundos
+    cy.get('[data-test="submit-button"]').click();
 
   })
 })
